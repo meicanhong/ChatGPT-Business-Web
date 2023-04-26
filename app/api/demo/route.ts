25bc9d37d3c4bdbcf0e169399544e01d11629d01
danny <canhong5357003@gmail.com>
2023-04-26 10:18:20 +0800

    init

--- app/api/demo/route.ts
+++ app/api/demo/route.ts
@@ -1,16 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import {createClient} from "redis";
+import { createClient } from "redis";
 
-const redis = require("redis");
 const client = createClient();
-client.on('error', err => console.log('Redis Client Error', err));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 export async function GET(req: NextRequest) {
-    await client.connect();
-    await client.set('key', 'value');
-    const value = await client.get('key');
-    await client.disconnect()
-    return NextResponse.json({
-        hello: value
-    });
+  await client.connect();
+  await client.set("key", "hello world");
+  const value = await client.get("key");
+  await client.disconnect();
+  return NextResponse.json({
+    hello: value,
+  });
 }
